d5af9697a5adf82903da460d27894d19c1730ed9
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-23 16:21:34 +0700

    Button navigasi ke loginpage dengan navigator pushNamed

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -46,9 +46,21 @@ class _RegisterPageState extends State<RegisterPage> {
                   return null;
                 },
               ),
-              ElevatedButton(onPressed: () {}, child: Text('Register'))
+              ElevatedButton(onPressed: () {}, child: Text('Register')),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                  // Navigator.pop(context);
+                  // Navigator.push(
+                  //   context, 
+                  //   MaterialPageRoute(builder: (context) => const LoginPage())
+                  // );
+                }, child: Text('Sudah punya akun? Login Sekarang!'),
+              )
             ],
-          ),)),
+          ),
+        )
+      ),
     );
   }
 }
